eeb7e35625b32a4873c17a0b0d68fdd95463ca6d
Leonardo Burati <leonardoburati@hotmail.com>
2021-02-02 21:20:17 -0300

    feat: create transaction repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +11,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transactions = await transactionRepository.find();
+
+    const { income, outcome } = await transactions.reduce(
+      (accumulator: Balance, transaction): Balance => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
